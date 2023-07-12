9ae2c0017c04a2fb13531d3729d5bab33df86d39
skhare <skhare@argusoft.com>
2023-07-12 10:38:41 +0530

    fixed need help in system code

--- medplat-ui/app/manage/systemcode/controllers/edit-code.modal.controller.js
+++ medplat-ui/app/manage/systemcode/controllers/edit-code.modal.controller.js
@@ -4,16 +4,42 @@
         $scope.init = function () {
             delete $scope.code;
             $scope.isValidCode = true;
+            $scope.isValidCodeDesc = true;
             $scope.listValueDetail = listValueDetail;
             $scope.code = {};
-            $scope.code.codeType = $scope.listValueDetail.codeTypes[0].codeType;
             $scope.code.tableId = $scope.listValueDetail.id;
+            $scope.code.codeType = $scope.listValueDetail.codeTypes[0];
             $scope.code.tableType = $scope.listValueDetail.tableType;
             $scope.onChangeCodeType($scope.code.codeType);
         };
 
-        $scope.isDisplayed = function() {
-            $scope.code.isDisplayed = !$scope.code.isDisplayed;
+        $scope.isDisplayed = function(data) {
+            
+            $scope.code.isDisplayed = false;
+            var url = '';
+            var messge = '';
+            if(data === 'SNOMED_CT'){
+                url = 'https://mlds.ihtsdotools.org';
+            } else if(data=== 'ICD_10'){
+                url = 'https://icd.who.int/browse10/2019/en';
+            } else if(data=== 'ICD_11'){
+                url = 'https://icd.who.int/browse11/l-m/en';
+            } else if(data=== 'LOINC'){
+                url = 'https://loinc.org/search/';
+            }
+            
+            if(url === ''){
+                message = 'Search URL not configured';
+            }else{
+                messge = 'You are about to navigate to the new tab ' + url+" . Do you want to continue?";
+            }
+            
+            if (confirm(messge)) {
+                window.open(url);
+            } else {
+                // OK;
+            }
+            
             // If need to clear
             // if (!$scope.code.isDisplayed){}
         };
@@ -31,28 +57,57 @@
             });
         };
 
-        $scope.verifyCode = function(){
-            if ($scope.code.code && $scope.code.code.length >= 3) {
-                if (!$scope.isFindingCodeDetails) {
-                    $scope.isFindingCodeDetails = true;
-                    Mask.show();
-                    SystemCodesService.getCodeDetailsByCodeAndType($scope.code.code,$scope.code.codeType).then((res) => {
-                            $scope.isValidCode = res.length > 0 ? true : false;
-                            $scope.isFindingCodeDetails = false;
-                            Mask.hide();
-                        });
-                    }
-            }
-            else{
-                $scope.isValidCode = false;
+        $scope.verifyCodeDesc = function () {
+            delete $scope.descErrorMsg;
+            if (!!$scope.code.description && $scope.code.description.length >= 255) {
+                $scope.descErrorMsg = 'Description length can be a maximum of 255';
+                $scope.isValidCodeDesc = false;
+            } else {
+                $scope.isValidCodeDesc = true;
             }
 
+        };
+        
+        $scope.verifyCode = function () {
+            delete $scope.codeErrorMsg;
+            if ($scope.code.code) {
+                if ($scope.code.code.length >= 4 && $scope.code.code.length <= 30) {
+                    $scope.isValidCode = true;
+                } else {
+                    if ($scope.code.code.length < 4) {
+                        $scope.codeErrorMsg = 'Code length can be a minimum of 4';
+                    }
+
+                    if ($scope.code.code.length > 30) {
+                        $scope.codeErrorMsg = 'Code length can be a maximum of 30';
+                    }
+                    $scope.isValidCode = false;
+                }
+                
+                
+//                if (!$scope.isFindingCodeDetails) {
+//                    $scope.isFindingCodeDetails = true;
+//                    Mask.show();
+//                    SystemCodesService.getCodeDetailsByCodeAndType($scope.code.code,$scope.code.codeType).then((res) => {
+//                            $scope.isValidCode = res.length > 0 ? true : false;
+//                            $scope.isFindingCodeDetails = false;
+//                            Mask.hide();
+//                        });
+//                    }
+            }
+            else{
+               
+                $scope.codeErrorMsg = 'Code can not be empty';
+                 $scope.isValidCode = false;
+                    
+            }
         }
 
         $scope.changeCodeOptions = function() {
             $scope.code.code = $scope.code.selectedCode.code;
             $scope.code.description = $scope.code.selectedCode.description || null;
             $scope.isValidCode = true;
+            delete $scope.codeErrorMsg;
         };
 
         $scope.close = function () {
@@ -82,15 +137,19 @@
 
         };
 
-        $scope.update = function () {
+        $scope.update = function (editCodeForm) {
+            $scope.verifyCode();
+            $scope.verifyCodeDesc();
             // If we will verify code here then will call API two time as on-blur it is already doing
-                if ($scope.code.codeType) {
+            if (editCodeForm.$valid) {
+                if ($scope.code.codeType && $scope.isValidCode && $scope.isValidCodeDesc) {
                     Mask.show();
                     SystemCodesService.saveOrUpdate($scope.code).then(() => {
                         toaster.pop('success', 'code updated successfully !');
                         $uibModalInstance.close('updated');
                     }).finally(Mask.hide);
                 }
+            }
         };
 
         $scope.init();
--- medplat-ui/app/manage/systemcode/controllers/manage-codes.controller.js
+++ medplat-ui/app/manage/systemcode/controllers/manage-codes.controller.js
@@ -1,5 +1,5 @@
 (function () {
-    function SystemCodeController(QueryDAO,ServerManageDAO,toaster, Mask, GeneralUtil,$scope, AuthenticateService, $uibModal) {
+    function SystemCodeController(QueryDAO, ServerManageDAO, toaster, Mask, GeneralUtil, $scope, AuthenticateService, $uibModal) {
         let ctrl = this;
 
         const _init = function () {
@@ -7,15 +7,13 @@
             AuthenticateService.getLoggedInUser().then(user => {
                 ctrl.loggedInUser = user.data;
             }, GeneralUtil.showMessageOnApiCallFailure).finally(Mask.hide);
-            ctrl.tables = [{key: 'OPD_LAB_TEST', name: 'Opd Lab Test'}, {key: 'LIST_VALUE', name: 'List Value'}];
+            ctrl.tables = [{ key: 'OPD_LAB_TEST', name: 'Opd Lab Test' }, { key: 'LIST_VALUE', name: 'List Value' }];
             ctrl.table = ctrl.tables[1];
             ctrl.retrieveForms();
             ctrl.retrieveSystemCodes();
-
             // ServerManageDAO.retrieveKeyValueBySystemKey('WHO_ICD_CODE_10_DEFAULT_START_CATEGOY').then((res) => {
             //     ctrl.startingCodeCategory = res.keyValue;
             // });
-
             // ServerManageDAO.retrieveKeyValueBySystemKey('WHO_ICD_CODE_10_DEFAULT_END_CATEGOY').then((res) => {
             //     ctrl.endingCodeCategory = res.keyValue;
             // });
@@ -35,12 +33,15 @@
                 Mask.hide();
             });
         };
+
         ctrl.onChangeTable = function (table) {
+            ctrl.clearValues();
             if (table.key === 'LIST_VALUE') {
                 ctrl.retrieveForms();
-            } else if (table.key === 'OPD_LAB_TEST') {
-                ctrl.retrieveOpdLabTest();
-            }
+            } 
+            // else if (table.key === 'OPD_LAB_TEST') {
+            //     ctrl.retrieveOpdLabTest();
+            // }
         };
 
         ctrl.retrieveFieldsForForm = function (form) {
@@ -82,18 +83,19 @@
             };
             Mask.show();
             QueryDAO.execute(queryDto).then((res) => {
-
                 ctrl.activeListValueDetails = res.result.map((data) => {
                     data.SYSTEM_CODE = JSON.parse(data.SYSTEM_CODE);
                     return data;
                 });
                 if (res.result.length > 0) {
                     ctrl.noOfColumns = res.result[0].codecount;
-                    ctrl.columnNames = res.result[0].codelist.map(val => val.codeType);
+                    if (res.result[0].codelist && res.result[0].codelist.length) {
+                        ctrl.columnNames = res.result[0].codelist.map(val => val.codeType);
+                    }
                 }
-
             }, GeneralUtil.showMessageOnApiCallFailure).finally(Mask.hide);
         };
+
         ctrl.toggleIsEditable = function (listValueDetail) {
             listValueDetail.isEditable = !listValueDetail.isEditable;
         };
@@ -102,68 +104,83 @@
             return systemCodes.find(val => val.codeType === codeType);
         };
 
-        ctrl.manageIcd = function() {
+        ctrl.showFlywayQueryOption = (systemCodes) => {
+            return systemCodes.filter(sc => sc.id).length ? true : false;
+        }
+
+        ctrl.showFlywayQuery = function (listValueDetail) {
+            let modalInstance = $uibModal.open({
+                templateUrl: 'app/manage/systemcode/views/show-flyway-query.modal.html',
+                controller: 'ShowFlywayQueryController',
+                windowClass: 'cst-modal',
+                controllerAs: 'flywayCtrl',
+                backdrop: 'static',
+                size: 'xl',
+                resolve: {
+                    listValueDetail: function () {
+                        return listValueDetail;
+                    },
+                    fieldKey: function () {
+                        return ctrl.newValue.field.field_key;
+                    }
+                }
+            });
+            modalInstance.result.then(() => {
+                console.log('Show Flyway Query Modal closed.');
+            }, () => {
+                console.log('Show Flyway Query Modal dismissed.');
+            });
+        };
+
+        ctrl.manageIcd = function () {
             let modalInstance = $uibModal.open({
                 templateUrl: 'app/manage/systemcode/views/manage-icd-snomed.model.html',
-                controllerAs : 'manageIcdCtrl',
-                controller: function($uibModalInstance,SystemCodesService,startingCodeCategory,endingCodeCategory,Mask,toaster,AuthenticateService,APP_CONFIG) {
+                controllerAs: 'manageIcdCtrl',
+                controller: function ($uibModalInstance, SystemCodesService, startingCodeCategory, endingCodeCategory, Mask, toaster, AuthenticateService, APP_CONFIG) {
                     let manageIcdCtrl = this;
                     manageIcdCtrl.startCodeFromSystem = startingCodeCategory;
                     manageIcdCtrl.endCodeFromSystem = endingCodeCategory;
-
-
-                    manageIcdCtrl.startingCodeCategory = startingCodeCategory  || 'A00'
-                    manageIcdCtrl.endingCodeCategory = endingCodeCategory || 'Z99'
-
+                    // manageIcdCtrl.startingCodeCategory = startingCodeCategory || 'A00'
+                    // manageIcdCtrl.endingCodeCategory = endingCodeCategory || 'Z99'
                     manageIcdCtrl.isDataInserting = false;
                     manageIcdCtrl.version = 10;
                     manageIcdCtrl.releaseYear = 2016;
 
+                    // manageIcdCtrl.process = function () {
+                    //     manageIcdCtrl.isDataInserting = true;
+                    //     if (manageIcdCtrl.manageIcdForm.$valid) {
+                    //         manageIcdCtrl.manageIcdForm.$setSubmitted();
+                    //         Mask.show();
+                    //         SystemCodesService.processICDCode(manageIcdCtrl.version, manageIcdCtrl.releaseYear, manageIcdCtrl.startingCodeCategory.toUpperCase(), manageIcdCtrl.endingCodeCategory.toUpperCase())
+                    //             .then((res) => {
+                    //                 toaster.pop('success', `${res["Total Processed"]} Data Inserted Successfully and took ${res["Time Taken"]}`);
+                    //                 $uibModalInstance.dismiss('cancel');
+                    //                 Mask.hide();
+                    //                 manageIcdCtrl.isDataInserting = false;
+                    //             })
+                    //             .catch((err) => {
+                    //                 toaster.pop('danger', 'An error Ocuured', err.data.message);
+                    //                 $uibModalInstance.dismiss('cancel');
+                    //                 Mask.hide();
+                    //                 manageIcdCtrl.isDataInserting = false;
+                    //             });
+                    //     }
+                    // }
 
-
-                    manageIcdCtrl.process = function() {
-                        manageIcdCtrl.isDataInserting = true;
-                        if (manageIcdCtrl.manageIcdForm.$valid) {
-                            manageIcdCtrl.manageIcdForm.$setSubmitted();
-                            Mask.show();
-                            SystemCodesService.processICDCode(manageIcdCtrl.version,manageIcdCtrl.releaseYear,manageIcdCtrl.startingCodeCategory.toUpperCase(),manageIcdCtrl.endingCodeCategory.toUpperCase())
-                                .then((res) => {
-                                    toaster.pop('success',`${res["Total Processed"]} Data Inserted Successfully and took ${res["Time Taken"]}`);
-                                    $uibModalInstance.dismiss('cancel');
-                                    Mask.hide();
-                                    manageIcdCtrl.isDataInserting = false;
-                                })
-                                .catch((err) => {
-                                    toaster.pop('danger','An error Ocuured',err.data.message);
-                                    $uibModalInstance.dismiss('cancel');
-                                    Mask.hide();
-                                    manageIcdCtrl.isDataInserting = false;
-                                });
-                        }
-                    }
-
-                    manageIcdCtrl.cancel = function() {
+                    manageIcdCtrl.cancel = function () {
                         $uibModalInstance.dismiss('cancel');
                     }
 
                     manageIcdCtrl.upload = function ($file, $event, $flow) {
                         manageIcdCtrl.responseMessage = {};
-                        if (($file.getExtension() === 'txt' || $file.getExtension() === 'csv')
-                            // && manageIcdCtrl.uploadFile.dateFormate
-                        ) {
+                        if (($file.getExtension() === 'txt' || $file.getExtension() === 'csv')) {
                             delete manageIcdCtrl.errorMessage;
                             manageIcdCtrl.isError = false;
-
                             manageIcdCtrl.isFormatError = false;
                             $flow.opts.target = APP_CONFIG.apiPath + '/systemcode/uploadcsv?codeType=SNOMED_CT';
                         } else {
                             manageIcdCtrl.isError = true;
-                            // if (!manageIcdCtrl.uploadFile.dateFormate) {
-                            //     toaster.pop('error', 'Please select Date format');
-                            // }
-                            // else {
                             toaster.pop('error', 'Please select correct file to upload!');
-                            // }
                             $flow.cancel();
                             return false;
                         }
@@ -182,7 +199,6 @@
                     };
 
                     manageIcdCtrl.filesStack = [];
-
                     manageIcdCtrl.uploadFn = function ($files, $event, $flow) {
                         if (manageIcdCtrl.filesStack.includes($flow.files[0].uniqueIdentifier)) {
                             toaster.pop('error', 'Duplicate files not allowed');
@@ -218,28 +234,25 @@
                     manageIcdCtrl.uploadError = function ($file, $message, $flow) {
                         toaster.pop('error', JSON.parse($message).message);
                     }
-
-
-                },
-                windowClass: 'cst-modal',
-                size: 'lg',
-                resolve: {
-                    startingCodeCategory : function() {
-                        return ctrl.startingCodeCategory;
-                    },
-                    endingCodeCategory : function() {
-                        return ctrl.endingCodeCategory;
-                    }
                 }
+                // ,
+                // windowClass: 'cst-modal',
+                // size: 'lg',
+                // resolve: {
+                //     startingCodeCategory: function () {
+                //         return ctrl.startingCodeCategory;
+                //     },
+                //     endingCodeCategory: function () {
+                //         return ctrl.endingCodeCategory;
+                //     }
+                // }
             });
-            modalInstance.result.then(function (val) {
-            }, function () { });
+            modalInstance.result.then(function () { }, function () { });
         }
 
         ctrl.editCode = function (listValueDetail) {
             listValueDetail.tableType = ctrl.table.key;
-            listValueDetail.codeTypes = ctrl.systemCodes;
-
+            listValueDetail.codeTypes = ctrl.systemCodes.map(val => val.codeType);
             let modalInstance = $uibModal.open({
                 templateUrl: 'app/manage/systemcode/views/edit-code.modal.html',
                 controller: 'EditCodeModalController',
@@ -252,21 +265,15 @@
                 }
             });
             modalInstance.result.then(function (val) {
-//                ctrl.onChangeTable(ctrl.table);
-
                 if (ctrl.table.key === 'LIST_VALUE') {
-//                    ctrl.retrieveForms();
-                    ctrl.retrieveActiveListValueDetails (ctrl.field);
-
-
-                } else if (ctrl.table.key === 'OPD_LAB_TEST') {
-//                    ctrl.retrieveOpdLabTest();
-                    ctrl.retrieveOpdLabTest();
-                }
-
+                    ctrl.retrieveActiveListValueDetails(ctrl.field);
+                } 
+                // else if (ctrl.table.key === 'OPD_LAB_TEST') {
+                //     ctrl.retrieveOpdLabTest();
+                // }
             }, function () { });
-
         };
+
         ctrl.clearValues = function () {
             ctrl.activeListValueDetails = [];
             ctrl.forms = [];
@@ -274,21 +281,88 @@
             delete ctrl.newValue;
         };
 
-        ctrl.retrieveOpdLabTest = function () {
-            let queryDto = {
-                code: 'retrieve_system_code_mapping_by_opd_lab_test',
-                parameters: {}
-            };
-            Mask.show();
-            QueryDAO.execute(queryDto).then((res) => {
-                ctrl.activeListValueDetails = res.result.map((data) => {
-                    data.SYSTEM_CODE = JSON.parse(data.SYSTEM_CODE);
-                    return data;
-                });
-            }, GeneralUtil.showMessageOnApiCallFailure).finally(Mask.hide);
-        }
-;
+        // ctrl.retrieveOpdLabTest = function () {
+        //     let queryDto = {
+        //         code: 'retrieve_system_code_mapping_by_opd_lab_test',
+        //         parameters: {}
+        //     };
+        //     Mask.show();
+        //     QueryDAO.execute(queryDto).then((res) => {
+        //         ctrl.activeListValueDetails = res.result.map((data) => {
+        //             data.SYSTEM_CODE = JSON.parse(data.SYSTEM_CODE);
+        //             return data;
+        //         });
+        //     }, GeneralUtil.showMessageOnApiCallFailure).finally(Mask.hide);
+        // };
+
         _init();
     }
     angular.module('imtecho.controllers').controller('SystemCodeController', SystemCodeController);
+})();
+
+
+(function () {
+    function ShowFlywayQueryController(listValueDetail, fieldKey, $uibModalInstance, toaster) {
+        let flywayCtrl = this;
+        flywayCtrl.listValueDetail = listValueDetail;
+        flywayCtrl.fieldKey = fieldKey;
+
+        flywayCtrl.init = function () {
+            flywayCtrl.query = '';
+            flywayCtrl.listValueDetail.SYSTEM_CODE.forEach(sc => {
+                flywayCtrl.query += `
+-- Flyway for ${sc.codeType} System Code of ${flywayCtrl.listValueDetail.name};
+
+DELETE FROM system_code_master WHERE id = '${sc.id}';
+
+INSERT INTO system_code_master
+    (id, table_id, table_type, code_type, code, parent_code, description, created_by, created_on, modified_by, modified_on)
+VALUES
+    ('${sc.id}', (
+        select id
+        from listvalue_field_value_detail
+        where is_active = true
+            and field_key = '${flywayCtrl.fieldKey}'
+            and value = '${flywayCtrl.listValueDetail.name}'
+        limit 1
+    ), '${sc.tableType}', '${sc.codeType}', '${sc.code}', ${sc.parentCode ? `'${sc.parentCode}'`  : null}, ${sc.description ? `'${sc.description}'`  : null}, ${sc.createdBy}, now(), ${sc.createdBy}, now());
+
+`;
+            })
+        };
+
+        flywayCtrl.copyQuery = function () {
+            const selBox = document.createElement('textarea');
+            selBox.style.position = 'fixed';
+            selBox.style.left = '0';
+            selBox.style.top = '0';
+            selBox.style.opacity = '0';
+            selBox.value = flywayCtrl.query;
+            document.body.appendChild(selBox);
+            selBox.focus();
+            selBox.select();
+            document.execCommand('copy');
+            document.body.removeChild(selBox);
+            $uibModalInstance.close();
+            toaster.pop('success', 'Query Copied');
+        };
+
+        flywayCtrl.downloadQuery = function () {
+            var a = window.document.createElement('a');
+            a.href = window.URL.createObjectURL(new Blob([flywayCtrl.query], { type: 'text/plain' }));
+            var name = "V" + new Date().getTime() + "__CHANGE_IN_" + flywayCtrl.listValueDetail.code + '.sql';
+            a.download = name;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            $uibModalInstance.close();
+        };
+
+        flywayCtrl.cancel = function () {
+            $uibModalInstance.close();
+        };
+
+        flywayCtrl.init();
+    }
+    angular.module('imtecho.controllers').controller('ShowFlywayQueryController', ShowFlywayQueryController);
 })();
