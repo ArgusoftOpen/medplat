069d4301c1e045032deb6910a8c83631a8285763
Anshul <asurpaithankar@argusoft.com>
2023-07-12 15:52:07 +0530

    Edited sample data deletion script

--- medplat-web/src/main/resources/db/util/sample_data_deletion.sql
+++ medplat-web/src/main/resources/db/util/sample_data_deletion.sql
@@ -5,22 +5,22 @@ ALTER TABLE imt_member DISABLE TRIGGER imt_member_delete_trigger ;
 DELETE FROM imt_member WHERE id IN (1,2) ;
 ALTER TABLE imt_member ENABLE TRIGGER imt_member_delete_trigger ;
 
-DELETE FROM imt_family WHERE id = 1 ;
+DELETE FROM imt_family WHERE family_id = 'FM/2023/1N' ;
 
 DELETE FROM mobile_menu_role_relation WHERE menu_id = 1 AND role_id = 2 ;
 
 DELETE FROM mobile_menu_master WHERE id = 1 ;
 
-DELETE FROM um_user_location WHERE user_id = 2 ;
+DELETE FROM um_user_location WHERE user_id != 1 ;
 
-DELETE FROM um_user where id = 2 ;
+DELETE FROM um_user where id != 1;
 
-DELETE FROM um_role_master WHERE id = 2 ;
+DELETE FROM um_role_master WHERE id != 1 ;
 
-DELETE FROM location_hierchy_closer_det WHERE parent_id IN (2,3,4,5,6,7) OR child_id IN (2,3,4,5,6,7);
+DELETE FROM location_hierchy_closer_det WHERE parent_id != 1 OR child_id != 1;
 
-DELETE FROM location_type_master WHERE id IN (2,3,4,5,6,7);
+DELETE FROM location_type_master WHERE id != 1;
 
-DELETE FROM location_wise_analytics WHERE loc_id IN (2,3,4,5,6,7) ;
+DELETE FROM location_wise_analytics WHERE loc_id != 1 ;
 
-DELETE FROM location_master WHERE id IN (2,3,4,5,6,7) ;
+DELETE FROM location_master WHERE id != 1 ;
