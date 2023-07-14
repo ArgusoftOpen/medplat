eeb0132799fb682c7e8488d8ab34e0d62c615eea
Devaansh Singh <139201890+devaanshSingh@users.noreply.github.com>
2023-07-14 10:41:35 +0530

    Fixed the healthInfra and other issues. (#19)

--- medplat-web/src/main/java/com/argusoft/medplat/web/healthinfra/controller/HealthInfrastructureController.java
+++ medplat-web/src/main/java/com/argusoft/medplat/web/healthinfra/controller/HealthInfrastructureController.java
@@ -2,8 +2,7 @@ package com.argusoft.medplat.web.healthinfra.controller;
 
 import com.argusoft.medplat.web.healthinfra.service.HealthInfrastructureService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/health-infra")
@@ -11,6 +10,12 @@ public class HealthInfrastructureController {
 
     @Autowired
     private HealthInfrastructureService healthInfrastructureService;
+
+    @RequestMapping(value = "/toggleactive/{id}", method = RequestMethod.POST)
+    public void toggleActive(@PathVariable Integer id,
+                             @RequestParam("state") String state) {
+        healthInfrastructureService.toggleActive(id, state);
+    }
 
 //    @GetMapping(value = "/get-health-infra-by-id/{id}")
 //    public HealthInfrastructureHFRDetails getHealthInfrastructureById(@PathVariable Integer id) {
