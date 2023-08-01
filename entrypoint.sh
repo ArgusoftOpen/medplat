d9fbe7774b7f35d320f5b41edfe59ce2a05cfa7a
nehil <nlimje@argusoft.com>
2023-08-01 12:14:49 +0530

    dockerfile changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-#mv apache-maven-3.2.5 /usr/web
-#mv jdk-13.0.2 /usr/web
-
 cd /usr/ui/medplat-ui
 
 # Run grunt task
 npm install
 bower install
 grunt medplat
-# tail -f /dev/null
 
 cd /usr/web
 mvn clean install -P docker -Dmaven.test.skip=true
 
-# cd /usr/web/target/
-mv /usr/web/target /usr/target/
-cd /usr/target/
+cd /usr/web/target/
 java -jar medplat-web-2.0.jar
